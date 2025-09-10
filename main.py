e79883f166d22f8fd72ae4b43b13c9a2b7b92494
djmartin2019 <djmartin2019@gmail.com>
2025-09-10 15:36:39 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from modules import create_documentation, list_documentation
+
+def main_menu():
+    while True:
+        print("\n===================")
+        print("---Docs To Read---")
+        print("1. Display List")
+        print("2. Add Doc")
+        print("3. Update Doc")
+        print("4. Delete Doc")
+        print("5. Quit")
+        print("===================")
+
+        try:
+            choice = int(input("Enter your choice: "))
+            print("===================\n")
+        except ValueError:
+            print("Please enter a number between 1-5")
+            print("===================\n")
+            continue
+
+        match choice:
+            case 1:
+                print("List of Docs")
+                list_documentation()
+            case 2:
+                print("Add Documentation")
+                create_documentation()
+            case 3:
+                print("Update Documentation Record")
+            case 4:
+                print("Delete Documentation")
+            case 5:
+                print("Exiting program")
+                break
+            case _:
+                print("Please select an option 1-5")
+
+if __name__ == "__main__":
+    main_menu()
